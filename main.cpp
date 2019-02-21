bd310d5043bdb0429d48111cb0af98de475ad23d
张凌云 <zhanglingyun1@xiaomi.com>
2019-02-21 15:14:44 +0800

    time

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
+#include <string>
 #include <iostream>
+#include <sys/time.h>
+#include <sstream>
+/**
+ * 当前时间秒数
+ * @return
+ */
+long long GetCurrentTime() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec;
+}
 
+/**
+ * 将string -> long long
+ */
+long long ToLong(std::string str) {
+    long long result;
+    std::istringstream is(str);
+    is >> result;
+    return result;
+}
+
+void show(int& put) {
+    put = 10;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << GetCurrentTime() << std::endl;
     return 0;
 }
